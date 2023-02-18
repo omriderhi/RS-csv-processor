bb3aeb01734218e6f91e2cabf1389315d843de9a
Omri Derhi <omriderhi@gmail.com>
2023-02-18 22:36:43 +0200

    added SRS processing ability

--- rs_csv_processor/resources/constants.py
+++ rs_csv_processor/resources/constants.py
@@ -7,3 +7,11 @@ class DefaultValues(str, Enum):
 
 class FieldNames:
     ndvi_raw_field_name_suffix = "NDVI_RAW"
+
+
+class SrsDefaultValues:
+    dat_date_format = '%Y-%m-%d %H:%M:%S'
+    fixed_date_format = '%d/%m/%Y'
+    fixed_date_time_format = '%d/%m/%Y %H:%M'
+    collection_hour = 10
+    collection_minutes = 00
